56e546b2e931516c0b34326abfb6914ebd47ee20
Alex <jgdfgfhhjjkk@gmail.com>
2024-03-01 13:51:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,98 @@
+
+// console.log(1);
+// let number = 5;
+// const leftBorderWidth = 1;
+// console.log(number);
+// number = 10;
+// console.log(number);
+
+//const obj = {
+//    a: 50
+//};
+//obj.a = 10;
+//console.log(obj);
+//console.log(name);
+//var name = 'Alex';
+//{
+//    var result = 50;
+//}
+//console.log(result);
+//const arr = [1, 2, 3];
+//console.log(arr[0]);
+
+// const obj1 = {
+//     'Anna' : 500,
+//     'alisa' : 800
+// };
+
+// const arr =['a', 'b', 'c'];
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// };
+// //console.log(arr[1]);
+// console.log(arrObj[1]);
+
+// const b = 'b';
+// //arrObj.b = '1234';
+// arrObj[b] = '1234';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+// //console.log(arr[1]);
+
+// const arr1 = ['a', 'b', 'c'];
+// arr1[3] = '3456';
+// arr1[3] = '000';
+
+// console.log(arr1);
+
+// const storeName = 'Ozon';
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Sasha', 'Arina', 'Alla'],
+//     products: {
+//         'конфеты': 100,
+//         'Шоколад': 200
+//     },
+//     open: true
+// }
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = + prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('как ваше имя', '');
+// answers[1] = prompt('Как ваша фамилия', '');
+// answers[2] = prompt('Сколько вам лет', '');
+
+// document.write(answers);
+
+// const  category = 'toys';
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = "Alex";
+// alert(`Привет, ${user}`);
+
+
+
+let incr = 10,
+    decr = 10;
+
+++incr
+--decr
+
+console.log(incr);
+console.log(decr);
+
+console.log(4*2 == 8);
+console.log(4*2 == '8');
+console.log(4*2 === '8');
+console.log(5%2);
