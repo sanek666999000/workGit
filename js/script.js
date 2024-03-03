aef75e4fc800a6a84023df1c44fdc5846274675a
Alex <jgdfgfhhjjkk@gmail.com>
2024-03-03 16:08:58 +0300

    Условия, операторы if...else, тернарный оператор, switch

--- js/script.js
+++ js/script.js
@@ -121,20 +121,91 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-      console.log(personalMovieDB);
+//       personalMovieDB.movies[a] = b;
+//       personalMovieDB.movies[c] = d;
+//       console.log(personalMovieDB);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+                                    //  Условия
+
+// if (4 == 4) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+
+// if (4 == 4) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+
+// if (4 == 6) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+
+//  const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok');
+// }
+
+
+//                         //  Тернарный оператор
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+                        // Свитч
+const num = 50;                        
+
+switch (num) {
+    case 49:
+        console.log('Не верно');
+        break;
+    case 100:
+        console.log('Не верно');            
+        break;
+     case 50:
+        console.log('В точку!');
+        break;
+     default:
+        console.log('Не в этот раз');
+        break;
+}
+
+
+
